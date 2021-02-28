e116215cf4be0cb340b22b9f4916a33283fcc6d2
alex <dobrovolya48@gmail.com>
2021-02-28 22:01:18 +0300

    mod js file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+    n = String(n);
+    let arr = [];
   
-}
+    for (let i = n.length - 1; i >= 0; i--) {
+        arr.push(n[i]);
+    }
+  
+    return arr.join('');
+  };
+  
